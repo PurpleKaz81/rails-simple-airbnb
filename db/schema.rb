a72948a5ed0c9eddb37dd97962806ebc04173958
Rafael Kasinski <rakasin@gmail.com>
2022-08-17 17:03:34 -0300

    Almost finished seeding

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_184956) do
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.text "description"
+    t.integer "price_per_night"
+    t.integer "number_of_guests"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.destroy_all
+
+puts 'Generating five flats'
+5.times do
+  flat = Flat.new(
+    name: Faker::Kpop.ii_groups,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 4),
+    price_per_night: "#{rand(0..1000).to_f}",
+    number_of_guests: Faker::Number.within(range: 1..4)
+  )
+  flat.save!
+end
+
+puts 'Finished!'
